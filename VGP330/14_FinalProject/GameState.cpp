8b3bf11167ee31ecb09f59ea384e8c9cf4566f49
KeithL <85144995+KeithL1m@users.noreply.github.com>
2024-03-09 12:24:22 -0800

    Added Final Project

---
+++ VGP330/14_FinalProject/GameState.cpp
@@ -0,0 +1,118 @@
+#include "GameState.h"
+
+using namespace KEIEngine;
+using namespace KEIEngine::Graphics;
+using namespace KEIEngine::KMath;
+using namespace KEIEngine::Input;
+
+void GameState::Initialize()
+{
+	mCamera.SetPosition({ 0.0f, 1.0f, -4.0f });
+	mCamera.SetLookAt({ 0.0f, 0.0f, 0.0f });
+
+	mStandardEffect.Initialize(L"../../Assets/Shaders/Standard.fx");
+	mStandardEffect.SetCamera(mCamera);
+	mStandardEffect.SetDirectionalLight(mDirectionalLight);
+	mStandardEffect.SetLightCamera(mShadowEffect.GetLightCamera());
+	mStandardEffect.SetShadowMap(mShadowEffect.GetDepthMap());
+
+	mTerrainEffect.Initialize();
+	mTerrainEffect.SetCamera(mCamera);
+	mTerrainEffect.SetDirectionalLight(mDirectionalLight);
+	mTerrainEffect.SetLightCamera(mShadowEffect.GetLightCamera());
+	mTerrainEffect.SetShadowMap(mShadowEffect.GetDepthMap());
+
+	ModelId modelId = ModelManager::Get()->LoadModel(L"../../Assets/Models/character/character.model");
+	mCharacter = CreateRenderGroup(modelId);
+
+	ModelId modelId2 = ModelManager::Get()->LoadModel(L"../../Assets/Models/character/character2.model");
+	mCharacter2 = CreateRenderGroup(modelId2);
+
+
+	mTerrain.Initialize(L"../../Assets/Textures/terrain/heightmap_512x512.raw", 20.0f);
+
+	mGround.meshBuffer.Initialize(mTerrain.GetMesh());
+	mGround.diffuseMapId = TextureManager::Get()->LoadTexture(L"terrain/dirt_seamless.jpg");
+	mGround.bumpMapId = TextureManager::Get()->LoadTexture(L"terrain/grass_2048.jpg");
+
+	mCharacterPosition.x = 10.0f;
+	mCharacterPosition.z = 10.0f;
+	mCharacterPosition.y = mTerrain.GetHeight(mCharacterPosition);
+	SetRenderGroupPosition(mCharacter, mCharacterPosition);
+
+	mCharacter2Position.x = 5.0f;
+	mCharacter2Position.z = 15.0f;
+	mCharacter2Position.y = mTerrain.GetHeight(mCharacter2Position);
+	SetRenderGroupPosition(mCharacter2, mCharacter2Position);
+
+};
+void GameState::Terminate()
+{
+	mGround.Terminate();
+	CleanupRenderGroup(mCharacter);
+	CleanupRenderGroup(mCharacter2);
+	mStandardEffect.Terminate();
+}
+void GameState::Update(float deltaTime)
+{
+	UpdateCameraControl(deltaTime);
+}
+
+void GameState::Render()
+{
+	mTerrainEffect.Begin();
+		mTerrainEffect.Render(mGround);
+	mTerrainEffect.End();
+
+	mStandardEffect.Begin();
+		DrawRenderGroup(mStandardEffect, mCharacter);
+		DrawRenderGroup(mStandardEffect, mCharacter2);
+	mStandardEffect.End();
+}
+
+void GameState::DebugUI()
+{
+	
+}
+
+void GameState::UpdateCameraControl(float deltaTime)
+{
+	auto input = Input::InputSystem::Get();
+	const float moveSpeed = input->IsKeyDown(KeyCode::LSHIFT) ? 10.0f : 1.0f;
+	const float turnSpeed = 0.1f;
+
+	if (input->IsKeyDown(KeyCode::W))
+	{
+		mCamera.Walk(moveSpeed * deltaTime);
+	}
+	else if (input->IsKeyDown(KeyCode::S))
+	{
+		mCamera.Walk(-moveSpeed * deltaTime);
+	}
+	if (input->IsKeyDown(KeyCode::D))
+	{
+		mCamera.Strafe(moveSpeed * deltaTime);
+	}
+	else if (input->IsKeyDown(KeyCode::A))
+	{
+		mCamera.Strafe(-moveSpeed * deltaTime);
+	}
+	if (input->IsKeyDown(KeyCode::E))
+	{
+		mCamera.Rise(moveSpeed * deltaTime);
+	}
+	else if (input->IsKeyDown(KeyCode::Q))
+	{
+		mCamera.Rise(-moveSpeed * deltaTime);
+	}
+	if (input->IsMouseDown(MouseButton::RBUTTON))
+	{
+		mCamera.Yaw(input->GetMouseMoveX() * turnSpeed * deltaTime);
+		mCamera.Pitch(input->GetMouseMoveY() * turnSpeed * deltaTime);
+	}
+
+	float characterHeight = 3.0f;
+	KMath::Vector3 cameraPos = mCamera.GetPosition();
+	cameraPos.y = mTerrain.GetHeight(cameraPos) + characterHeight;
+	mCamera.SetPosition(cameraPos);
+}
---
+++ VGP330/14_FinalProject/GameState.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <../KEIEngine/Inc/KEIEngine.h>
+
+
+class GameState : public KEIEngine::AppState
+{
+public:
+	~GameState() = default;
+	void Initialize() override;
+	void Terminate() override;
+	void Render() override;
+	void Update(float deltaTime) override;
+	void DebugUI() override;
+
+private:
+	void UpdateCameraControl(float deltaTime);
+
+	KEIEngine::Graphics::Camera mCamera;
+	KEIEngine::Graphics::DirectionalLight mDirectionalLight;
+	KEIEngine::Graphics::StandardEffect mStandardEffect;
+	KEIEngine::Graphics::TerrainEffect mTerrainEffect;
+	KEIEngine::Graphics::ShadowEffect mShadowEffect;
+
+	KEIEngine::Graphics::RenderGroup mCharacter;
+	KEIEngine::Graphics::RenderGroup mCharacter2;
+	KEIEngine::Graphics::RenderObject mGround;
+	KEIEngine::Graphics::Terrain mTerrain;
+
+	KEIEngine::KMath::Vector3 mCharacterPosition;
+	KEIEngine::KMath::Vector3 mCharacter2Position;
+};
